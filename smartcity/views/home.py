289bd2672682adf52fab3614a9eadb698fc7f285
Ladislav Vašina <ladislavvasina@gmail.com>
2022-11-22 22:04:10 +0100

    Added user profile

--- smartcity/views/home.py
+++ smartcity/views/home.py
@@ -3,14 +3,20 @@ from flask_login import current_user, login_required
 from smartcity.views import roles_required
 from smartcity.models import Role, User
 
+from .admin_forms import EditUser
+
 home_bp = Blueprint("home", __name__)
 
 
-@home_bp.route("/", methods=["GET"])
+@home_bp.route("/", methods=["GET", "POST"])
 def index():
     roles = Role.query.all()
     if current_user.is_anonymous:
         users = None
     else:
         users = User.query.filter_by(email=current_user.email).first()
-    return render_template("public/index.html", roles=roles, users=users, current_user=current_user)
+    if current_user.is_authenticated:
+        edit_form = EditUser(name=users.name, surname=users.surname, email=users.email, role=users.role[0].name)
+        return render_template("public/index.html", roles=roles, users=users, current_user=current_user, form=edit_form)
+    else:
+        return render_template("public/index.html", roles=roles, users=users, current_user=current_user)
