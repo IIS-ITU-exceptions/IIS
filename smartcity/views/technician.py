daeb1b437ec13c90fd6957362f428e73928d1b04
bígl <plavec1@gmail.com>
2022-11-22 19:53:29 +0100

    Technician task view

--- smartcity/views/technician.py
+++ smartcity/views/technician.py
@@ -16,15 +16,13 @@ def assigned_tasks():
     service_tasks = ServiceTask.query.join(ServiceTaskUsers).filter(ServiceTaskUsers.user_id == current_user.id).all()
     return render_template("technician/assigned_tasks.html", current_user=current_user, service_tasks=service_tasks)
 
-@technician_bp.route("/task_view", methods=["GET"])
+@technician_bp.route("/task_view", methods=["GET", "POST"])
 @login_required
 @roles_required(["technician"])
 def task_view():
     if request.method == "GET":
-        ticket_comments = Comment.query.filter_by(ticket_id=request.args.get("ticketId")).all()
-        try:
-            selected_ticket_id = int(request.args.get("ticketId"))
-        except Exception as e:
-            return render_template("204.html")
-    tickets = Ticket.query.filter(Ticket.assignee_id == current_user.id, Ticket.state != 'New').all()
-    return render_template("technician/task_view.html", current_user=current_user, tickets=tickets)
+        ticket = Ticket.query.join(ServiceTask).filter(Ticket.id == request.args.get("parent")).all()
+        comments = Comment.query.filter(Comment.ticket_id == ticket[0].id).all()
+        task = ServiceTask.query.filter(ServiceTask.id == request.args.get("taskID")).all() ###????
+    return render_template("technician/task_view.html", current_user=current_user, ticket=ticket,
+                           comments=comments, task=task)
